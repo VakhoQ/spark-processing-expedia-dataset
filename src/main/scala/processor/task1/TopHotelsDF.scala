5c1a04de48f3ab0d792de88f31ac64cc9425fb4b
vq <vakhtang.koroghlishvili@gmail.com>
2019-10-02 02:40:53 -0400

    - code refactoring

--- src/main/scala/processor/task1/TopHotelsDF.scala
+++ src/main/scala/processor/task1/TopHotelsDF.scala
@@ -27,6 +27,22 @@ class TopHotelsDF  extends BaseProcessor{
       .sort(desc("uniq_hotel_popularity_index"))
       .limit(3)
 
+    /* The similar result but not optimized:
+    df.createOrReplaceTempView("TRAIN_FROM_AVRO_SCHEMA")
+    val sql : String =
+      "SELECT HOTELS.HOTEL, \n" +
+        "COUNT(HOTELS.HOTEL) AS COUNT \n" +
+        "FROM (\n" +
+        " SELECT  \n" +
+        " CONCAT('  ',hotel_continent, '  ',hotel_country,' ' ,hotel_market) AS HOTEL \n" +
+        " FROM TRAIN_FROM_AVRO_SCHEMA \n" +
+        " where SRCH_ADULTS_CNT=2\n" +
+        ") HOTELS \n" +
+        "GROUP BY HOTELS.HOTEL \n" +
+        "ORDER BY COUNT DESC LIMIT 3";
+     val sqlDF = spark.sql(sql)
+     */
+
 
     if(isResultLoEnabled){
       result.show()
